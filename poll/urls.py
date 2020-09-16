2bc2c0c752e739c5e2e17bc443e52e8fded1dd54
Bhuban Ghimire <bhubanghimire3130@gmail.com>
2020-09-16 21:08:44 +0545

    part 3

--- poll/urls.py
+++ poll/urls.py
@@ -1,3 +1,12 @@
+from .views import detail,results,vote,index
+from django.urls import path
+
+
+app_name = 'poll'
 urlpatterns = [
+    path('', index, name='index'),
+    path('poll/<int:question_id>',detail,name="detail"),
+    path('result/<int:question_id>',results,name="result"),
+    path('vote/<int:question_id>',vote,name="vote"),
     
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    output = Question.objects.order_by('pub_date')
+    # output = ', '.join([q.question_text for q in output])
+    template = loader.get_template('index.html')
+    context = {
+        'latest_question_list':output,
+    }
+    return render(request,'index.html',context)
+
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except:
+    #     raise http404("question not found")
+    
+
+
+    #this is the eqivalent of above
+
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question':question
+    }
+
+    return render(request,'detail.html',context)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
